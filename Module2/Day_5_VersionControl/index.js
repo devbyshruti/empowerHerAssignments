7790b4b2002a98a2a2d4bfc8e7d1ae03d2d7857d
devbyshruti <shruti.vpatel762@gmail.com>
2025-11-14 21:57:28 +0530

    Add Prime Number Function

---
+++ Module2/Day_5_VersionControl/index.js
@@ -0,0 +1,9 @@
+function isPrime(num) {
+    if (num<=1) return false;
+    if (num==2) return true;
+
+    for(let i=2;i<num;i++){
+        if (num%i==0)
+            return false;
+    } return true;
+} console.log(isPrime(7));
